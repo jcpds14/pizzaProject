63d926888409376086e556ae86a462858e73d00b
Jean Carlos <jcpds14@gmail.com>
2023-01-16 22:10:04 -0300

    add pizzas info and images to interface

--- Front/assets/js/script.js
+++ Front/assets/js/script.js
@@ -2,7 +2,12 @@ const q = el => document.querySelector(el);
 const qAll = el => document.querySelectorAll(el);
 
 pizzaJson.map((item, index) => {
-    let pizzaItem = q(".models .pizza-item").cloneNode(true);
+    const pizzaItem = q(".models .pizza-item").cloneNode(true);
+
+    pizzaItem.querySelector(".pizza-item--img img").src = item.img;
+    pizzaItem.querySelector(".pizza-item--price").innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector(".pizza-item--name").innerHTML = item.name;
+    pizzaItem.querySelector(".pizza-item--desc").innerHTML = item.description;
 
     q(".pizza-area").appendChild(pizzaItem);
 });
